12295859d7c92e353a3f66178fc0b2c71a40a78b
Iramirez108 <ivan.ramirez10208@gmail.com>
2022-09-15 00:25:45 -0500

    getting api and seed folders ready

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
-const userRoutes = require('./userRoutes');
-const projectRoutes = require('./projectRoutes');
+const userRoutes = require('./userData-routes');
+const projectRoutes = require('./projectData-routes');
 
 router.use('/users', userRoutes);
 router.use('/projects', projectRoutes);
