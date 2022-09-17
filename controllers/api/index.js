7e806096a0ecae1f57eda5b19a216f10a385ae47
Iramirez108 <ivan.ramirez10208@gmail.com>
2022-09-17 15:15:44 -0500

    api routes made

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
-const userRoutes = require('./userData-routes');
-const projectRoutes = require('./projectData-routes');
+const userRoutes = require('./userRoutes');
+const postRoutes = require('./postRoutes');
+const commentRoutes = require('./commentRoutes');
 
 router.use('/users', userRoutes);
-router.use('/projects', projectRoutes);
+router.use('/post', postRoutes);
+router.user('/comment', commentRoutes);
 
 module.exports = router;
---
+++ controllers/api/postRoutes.js
@@ -0,0 +1,61 @@
+const router = require('express').router();
+const { Post } = require('../../models');
+
+
+router.post('/', async (req, res) => {
+    try {
+      const newProject = await Project.create({
+        ...req.body,
+        user_id: req.session.user_id,
+      });
+  
+      res.status(200).json(newProject);
+    } catch (err) {
+      res.status(400).json(err);
+    }
+});
+  
+
+  router.put('/:id', withAuth, async (req, res) => {
+    try {
+      const updateData = await Post.update(
+        {
+        ...req.body,
+        },
+        {
+          where: {
+            id: req.params.id,
+          },
+        }
+      );
+      if (!updateData) {
+        res.status(404).json({ message: 'No post found with this id!' });
+        return;
+      }
+      res.status(200).json(updateData);
+    } catch (err) {
+      res.status(400).json(err.message);
+    }
+});
+
+router.delete('/:id', async (req, res) => {
+    try {
+      const projectData = await Project.destroy({
+        where: {
+          id: req.params.id,
+          user_id: req.session.user_id,
+        },
+      });
+  
+      if (!projectData) {
+        res.status(404).json({ message: 'No project found with this id!' });
+        return;
+      }
+  
+      res.status(200).json(projectData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+  
+  module.exports = router;
