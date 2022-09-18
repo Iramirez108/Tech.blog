9776657481fc7d64bce7bd1e4242878c7d597776
Iramirez108 <ivan.ramirez10208@gmail.com>
2022-09-17 23:21:14 -0500

    server setting

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
-
+const hbs = exphbs.create({ helpers });
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const helpers = require('./utils/helpers');
@@ -14,20 +14,15 @@ const PORT = process.env.PORT || 3001;
 // Set up sessions with cookies
 const sess = {
   secret: 'Super secret secret',
-  cookie: {
-    // Stored in milliseconds
-    maxAge: 24 * 60 * 60 * 1000, // expires after 1 day
-  },
+  cookie: { maxAge: 36000 },
   resave: false,
   saveUninitialized: true,
   store: new SequelizeStore({
-    db: sequelize,
-  }),
+      db: sequelize
+  })
 };
-
 app.use(session(sess));
 
-const hbs = exphbs.create({ helpers });
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
@@ -39,9 +34,5 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () =>
-    console.log(
-      `\nServer running on port ${PORT}. Visit http://localhost:${PORT} and create an account!`
-    )
-  );
+  app.listen(PORT, () => console.log('Now listening'));
 });
